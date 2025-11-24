5d148f2480d0244f4e6958d9a4d6001f1f8c4aad
lgs <363703943@qq.com>
2025-11-24 23:46:05 +0800

    new

--- pages/tap_by_trials_tower.py
+++ pages/tap_by_trials_tower.py
@@ -13,54 +13,74 @@ class Tap_By_Trials_Tower:
 
     # 斗塔
     def Page_Trials_Tower(self, duration=300):
-        window_size = self.driver.get_window_size()
+        # window_size = self.driver.get_window_size()
         # 初始化点击主城
         self.tap_by_percent(5)
-        page_name = "./page_png/home.png"
-        is_home = self.get_snapshot(file_path=page_name, compare=True)
+        is_home = self.get_snapshot(file_path="../page_png/home.png", compare=True)
         if is_home is True:
             # 点击斗塔入口
-            x = window_size['width'] * 0.4
-            y = window_size['height'] * 0.5
-            self.driver.tap([(x, y)], duration)
-            print(f"📍 已通过坐标 ({x}, {y})点击斗塔入口")
+            self.utils.coordinates(width=0.4, height=0.5)
+
+            # x = window_size['width'] * 0.4
+            # y = window_size['height'] * 0.5
+            # self.driver.tap([(x, y)], duration)
+            # print(f"📍 已通过坐标 ({x}, {y})点击斗塔入口")
 
             for i in range(3):
                 # 点击挑战
                 time.sleep(1)
-                x = window_size['width'] * 0.5
-                y = window_size['height'] * 0.85
-                self.driver.tap([(x, y)], duration)
-                print(f"📍 已通过坐标 ({x}, {y})点击挑战")
+
+                self.utils.coordinates(width=0.5, height=0.85)
+
+
+                # x = window_size['width'] * 0.5
+                # y = window_size['height'] * 0.85
+                # self.driver.tap([(x, y)], duration)
+                # print(f"📍 已通过坐标 ({x}, {y})点击挑战")
                 # 点击出战
                 time.sleep(1)
-                x = window_size['width'] * 0.66
-                y = window_size['height'] * 0.85
-                self.driver.tap([(x, y)], duration)
-                print(f"📍 已通过坐标 ({x}, {y})点击出战")
+
+                self.utils.coordinates(width=0.66, height=0.85)
+
+                #
+                # x = window_size['width'] * 0.66
+                # y = window_size['height'] * 0.85
+                # self.driver.tap([(x, y)], duration)
+                # print(f"📍 已通过坐标 ({x}, {y})点击出战")
+
                 time.sleep(1.3)
-                x = window_size['width'] * 0.95
-                y = window_size['height'] * 0.84
-                self.driver.tap([(x, y)], duration)
-                print(f"📍 已通过坐标 ({x}, {y})点击跳过战斗")
+                self.utils.coordinates(width=0.95, height=0.84)
+
+                # x = window_size['width'] * 0.95
+                # y = window_size['height'] * 0.84
+                # self.driver.tap([(x, y)], duration)
+                # print(f"📍 已通过坐标 ({x}, {y})点击跳过战斗")
 
                 time.sleep(2)
-                x = window_size['width'] * 0.25
-                y = window_size['height'] * 0.87
-                self.driver.tap([(x, y)], duration)
-                print(f"📍 已通过坐标 ({x}, {y})确定")
+                self.utils.coordinates(width=0.25, height=0.87)
+
+                # x = window_size['width'] * 0.25
+                # y = window_size['height'] * 0.87
+                # self.driver.tap([(x, y)], duration)
+                # print(f"📍 已通过坐标 ({x}, {y})确定")
 
             # 点击每日奖励
-            x = window_size['width'] * 0.95
-            y = window_size['height'] * 0.3
-            self.driver.tap([(x, y)], duration)
-            print(f"📍 已通过坐标 ({x}, {y})点击每日奖励")
+
+            self.utils.coordinates(width=0.95, height=0.3)
+
+            # x = window_size['width'] * 0.95
+            # y = window_size['height'] * 0.3
+            # self.driver.tap([(x, y)], duration)
+            # print(f"📍 已通过坐标 ({x}, {y})点击每日奖励")
             time.sleep(0.5)
             # 点击领取
-            x = window_size['width'] * 0.5
-            y = window_size['height'] * 0.8
-            self.driver.tap([(x, y)], duration)
-            print(f"📍 已通过坐标 ({x}, {y})点击领取")
+
+            self.utils.coordinates(width=0.5, height=0.8)
+
+            # x = window_size['width'] * 0.5
+            # y = window_size['height'] * 0.8
+            # self.driver.tap([(x, y)], duration)
+            # print(f"📍 已通过坐标 ({x}, {y})点击领取")
 
         else:
             print("斗塔流程异常")
--- pages/tap_by_vip.py
+++ pages/tap_by_vip.py
@@ -12,40 +12,47 @@ class Tap_By_VIP:
         self.tap_by_percent = Tap_By_Percent()
 
     # 领取vip经验
-    def Page_Vip(self, duration=300):
+    def Page_Vip(self, duration=10):
         """按屏幕百分比点击"""
-        window_size = self.driver.get_window_size()
+        # window_size = self.driver.get_window_size()
         # 点击主城
         self.tap_by_percent.Page_Percent(5)
-        page_name = "./page_png/home.png"
+        page_name = "../page_png/home.png"
         is_home = self.get_snapshot(file_path=page_name, compare=True)
         if is_home is True:
 
             # 点击vip入口
-            x = window_size['width'] * 0.07
-            y = window_size['height'] * 0.1
-            self.driver.tap([(x, y)], duration)
-            print(f"📍 已通过坐标点击 ({x:.0f}, {y:.0f}) vip入口")
-            time.sleep(1)
+            self.utils.coordinates(width=0.07, height=0.1)
+
+            # x = window_size['width'] * 0.07
+            # y = window_size['height'] * 0.1
+            # self.driver.tap([(x, y)], duration)
+            # print(f"📍 已通过坐标点击 ({x:.0f}, {y:.0f}) vip入口")
+            # time.sleep(1)
 
             # 点击宝箱
-            x = window_size['width'] * 0.94
-            y = window_size['height'] * 0.24
-            self.driver.tap([(x, y)], duration)
-            print(f"📍 已通过坐标点击 ({x:.0f}, {y:.0f}) 宝箱")
-            time.sleep(1)
+            self.utils.coordinates(width=0.94, height=0.24)
+
+            # x = window_size['width'] * 0.94
+            # y = window_size['height'] * 0.24
+            # self.driver.tap([(x, y)], duration)
+            # print(f"📍 已通过坐标点击 ({x:.0f}, {y:.0f}) 宝箱")
+            # time.sleep(1)
 
             # 点击领取
-            x = window_size['width'] * 0.5
-            y = window_size['height'] * 0.58
-            self.driver.tap([(x, y)], duration)
-            print(f"📍 已通过坐标点击 ({x:.0f}, {y:.0f}) 点击领取按钮")
-            time.sleep(1)
+            self.utils.coordinates(width=0.5, height=0.58)
+
+            # x = window_size['width'] * 0.5
+            # y = window_size['height'] * 0.58
+            # self.driver.tap([(x, y)], duration)
+            # print(f"📍 已通过坐标点击 ({x:.0f}, {y:.0f}) 点击领取按钮")
+            # time.sleep(1)
 
             print("vip经验已领取")
-            return "已领取vip经验"
+            return True
         else:
             print("流程异常跳过vip任务...")
+            return False
 
 
 
