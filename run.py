0a46af8b33b2626960162bc2594f393c76d48747
lgs <363703943@qq.com>
2025-12-04 23:42:20 +0800

    封装重构

--- run.py
+++ run.py
@@ -17,7 +17,8 @@ class Run():
     def get_run(self):
         try:
             # self.utils.get_snapshot(file_path="./page_png/Dungeon.png")
-            self.utils.coordinates(width=0.2, height=0.96)
+            for i in range(10):
+                self.utils.coordinates(width=0.2, height=0.96)
         except Exception as e:
             print(e)
         finally:
